85a9c338f35fa2ca585cb07cb177058d121280eb
jmcabornero <jmcabornero@gmail.com>
2021-05-19 18:58:21 +0200

    tiles medio arreglado
    Former-commit-id: 640adf6450707dee4e06bf9862f3247d5d883163

--- EscenarioTiles.h
+++ EscenarioTiles.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "ETSIDI.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+//using ETSIDI::SpriteSequence;
+
 class EscenarioTiles
 {
 private:
 	int coordenada_x, coordenada_y;
-	int MatrizMapa[5][5] = {
-		{1,1,1,1,1},
-		{1,1,1,1,2},
-		{1,1,1,1,2},
-		{1,1,1,1,2},
-		{1,1,1,1,1} };
 
 public:
-	void Dibuja();
+	//void Dibuja();
+	void mapa();
+	//SpriteSequence loco{ "C:/Users/Usuario/source/repos/Juego/bin/imagenes/terrain.png", 21, 23 };
 };
 
 
