072964b72c422cea923faa366467989917a85e53
Jesús <jesus.marcos.torero@alumnos.upm.es>
2021-05-28 17:39:42 +0200

    Update protagonista
    Former-commit-id: 6022a3467194cbf74a1fed7c52a4b39d757df3e9

--- Protagonista.cpp
+++ Protagonista.cpp
@@ -6,6 +6,10 @@ Protagonista::Protagonista()
 {
 	sprite.setCenter(1, 0);
 	sprite.setSize(1, 1);
+	spriteR.setCenter(1, 0);
+	spriteR.setSize(1, 1);
+	spriteL.setCenter(1, 0);
+	spriteL.setSize(1, 1);
 	posicion.x = 5;posicion.y = 5;velocidad = 0;velocidad = 0;
 }
 
@@ -16,13 +20,9 @@ void Protagonista::Dibuja()
 	glColor3f(1.0f, 0.0f, 0.0f);
 	//glutSolidSphere(altura, 20, 20);
 //gestion de direccion y animacion
-	if (velocidad.x > 0.01)sprite.flip(false, false);
-	if (velocidad.x < -0.01)sprite.flip(true, false);
-	if ((velocidad.x < 0.01) && (velocidad.x > -0.01))
-		sprite.setState(0);
-	else if (sprite.getState() == 0)
-		sprite.setState(1, false);
-	sprite.draw();
+	if (velocidad.x > 0.01)spriteR.draw();
+	if (velocidad.x < -0.01)spriteL.draw();
+	if ((velocidad.x < 0.01) && (velocidad.x > -0.01))sprite.draw();
 	glPopMatrix();
 }
 
@@ -30,12 +30,18 @@ void Protagonista::Mueve(float t)
 {
 	posicion = posicion + velocidad * t;
 	sprite.loop();
+	spriteR.loop();
+	spriteL.loop();
 }
 
 void Protagonista::setTam(float anchura, float altura)
 {
 	sprite.setCenter(1, 0);
 	sprite.setSize(anchura, altura);
+	spriteR.setCenter(1, 0);
+	spriteR.setSize(anchura, altura);
+	spriteL.setCenter(1, 0);
+	spriteL.setSize(anchura, altura);
 }
 
 void Protagonista::setVel(float vx, float vy)
