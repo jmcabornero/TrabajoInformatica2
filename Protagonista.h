6022a3467194cbf74a1fed7c52a4b39d757df3e9
Jesús <jesus.marcos.torero@alumnos.upm.es>
2021-05-28 17:39:42 +0200

    Update protagonista

--- Protagonista.h
+++ Protagonista.h
@@ -10,7 +10,9 @@ private:
 	Vector2D posicion, velocidad;
 public:
 	Protagonista();
-	SpriteSequence sprite{ "imagenes/pangPlayer.png", 5 };
+	SpriteSequence sprite{ "imagenes/Player_reposo.png", 5 };
+	SpriteSequence spriteR{ "imagenes/Player_derecha.png", 5 };
+	SpriteSequence spriteL{ "imagenes/Player_izquierda.png", 5 };
 	void Dibuja();
 	void Mueve(float t);
 	void setTam(float anchura, float altura);
@@ -21,4 +23,3 @@ public:
 	Vector2D GetPos();
 	Vector2D GetVel();
 }; 
-
