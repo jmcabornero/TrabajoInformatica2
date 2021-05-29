cc3d3e7d9f95e0ad648298fe804ba1b93d7989f5
Jesús <jesus.marcos.torero@alumnos.upm.es>
2021-05-29 11:18:27 +0200

    Disparo && abstraccion movimiento
    Former-commit-id: 4be28f945bdcb7739e376dae6f1a909d1d85fd81

--- Disparo.cpp
+++ Disparo.cpp
@@ -1,1 +1,28 @@
 #include "Disparo.h"
+#include "freeglut.h"
+
+Disparo::Disparo() {
+	sprite.setCenter(0.5, 0.5);
+	sprite.setSize(1, 1);
+	setPos(5, 5);
+	setVel(5, 5);
+	setRadio(0.25);
+}
+
+Disparo::~Disparo() {
+
+}
+
+void Disparo::dibuja() {
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 1);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	//glutSolidSphere(radio, 20, 20);
+	sprite.draw();
+	glPopMatrix();
+}
+
+void Disparo::mueve(float t) {
+	ObjetosMovimiento::mueve(t);
+	sprite.loop();
+}
--- Disparo.h
+++ Disparo.h
@@ -1,5 +1,23 @@
 #pragma once
-class Disparo
+#include "Vector2D.h"
+#include "ETSIDI.h"
+#include "ObjetosMovimiento.h"
+#include "Protagonista.h"
+using ETSIDI::SpriteSequence;
+
+class Disparo:public ObjetosMovimiento
 {
+public:
+	SpriteSequence sprite{ "imagenes/Disparo1.png", 3 };
+	//SpriteSequence estela;
+	Disparo();
+	virtual ~Disparo();
+private:
+	float radio;
+public:
+	void setRadio(float r) { radio = r; }
+	float getRadio() { return radio; }
+	void dibuja();
+	void mueve(float t);
 };
 
