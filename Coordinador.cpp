3f013976aa2241b226dd534cdf9fa0400b61bfa7
josevicenteguillenmainez <47786528+josevicenteguillenmainez@users.noreply.github.com>
2021-06-05 20:01:34 +0200

    Coordinador tras haber chupado, empezar de nuevo

---
+++ Coordinador.cpp
@@ -0,0 +1,31 @@
+#include "Coordinador.h"
+
+Coordinador::Coordinador()
+{
+	estado = INICIO;
+};
+
+Coordinador::~Coordinador()
+{
+
+};
+
+void Coordinador::Inicializa()
+{
+
+};
+
+void Coordinador::Dibuja()
+{
+
+};
+
+void Coordinador::Mueve()
+{
+
+};
+
+void Coordinador::Tecla(unsigned char key)
+{
+
+};
