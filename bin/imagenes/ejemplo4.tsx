b2f8f34a7674430567890e1b5cb9474319245a55
jmcabornero <jmcabornero@gmail.com>
2021-06-06 19:29:26 +0200

    salta el mensaje en frente de cualquier buda

--- bin/imagenes/ejemplo4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ejemplo4" tilewidth="28" tileheight="28" tilecount="1024" columns="32">
- <image source="kisspng-tile-based-video-game-tiled-sprite-map-rpg-5ab7a4497ede95.9530021915219845855197.jpg" width="900" height="900"/>
-</tileset>
